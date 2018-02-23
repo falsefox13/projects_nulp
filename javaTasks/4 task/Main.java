1fcb555f0146576bb4b67ad6afea9487886daeb3
falsefox13 <33019423+falsefox13@users.noreply.github.com>
2018-02-23 10:48:06 +0200

    fixed 4 task

--- javaTasks/4 task/Main.java
+++ javaTasks/4 task/Main.java
@@ -1,10 +1,10 @@
 package stringprocessor;
 
-import java.io.IOException;
+        import java.io.IOException;
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        StringProcessor proc = new StringProcessor();   
+        StringProcessor proc = new StringProcessor();
         String text = proc.readInputText();
         proc.showResult(proc.processText(text));
     }
