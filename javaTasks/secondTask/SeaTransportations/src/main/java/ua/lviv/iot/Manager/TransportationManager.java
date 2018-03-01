98e22a2d739a64a1d12c0b5742e2e75fe27d5f73
falsefox13 <33019423+falsefox13@users.noreply.github.com>
2018-03-01 14:21:41 +0200

    fixes to 5th lab

--- javaTasks/secondTask/SeaTransportations/src/main/java/ua/lviv/iot/Manager/TransportationManager.java
+++ javaTasks/secondTask/SeaTransportations/src/main/java/ua/lviv/iot/Manager/TransportationManager.java
@@ -7,64 +7,69 @@ import ua.lviv.iot.Models.Ship;
 import java.util.ArrayList;
 
 /**
- * Class that handle transportations, 
- * can select the cheapest or the fastest propositions
+ * Class that handle transportations,
+ * can select the cheapest or the fastest propositions.
+ *
  * @author falsefox13
  */
 public class TransportationManager {
     private ArrayList<Ship> availableShips = new ArrayList<Ship>();
     private ArrayList<SeaTransportation> transportations = new ArrayList<SeaTransportation>();
-    
-    public TransportationManager() {}
-    
+
+    public TransportationManager() {
+    }
+
     /**
-     * Get an index of first suitable available ship
-     * @param cargo needed cargo
-     * @return index of needed Ship
+     * Get an index of first suitable available ship.
+     *
+     * @param cargo needed cargo.
+     * @return index of needed Ship.
      */
-    private int getIndexOfNeededShip(int cargo) {
+    private int getIndexOfNeededShip(final int cargo) {
         int indexOfNeededShip = -1;
-        if(availableShips.isEmpty() != true) {
-            for(int i = 0; i < availableShips.size(); i++) {
-                if(availableShips.get(i).getCapacity() >= cargo) {
+        if (availableShips.isEmpty() != true) {
+            for (int i = 0; i < availableShips.size(); i++) {
+                if (availableShips.get(i).getCapacity() >= cargo) {
                     indexOfNeededShip = i;
                 }
             }
         }
         return indexOfNeededShip;
     }
-    
+
     /**
-     * Select the first cheapest available transportation
-     * @return String with description of the transportation
+     * Select the first cheapest available transportation.
+     *
+     * @return String with description of the transportation.
      */
-    public String selectCheapest(Port sender, Port dest, int maxPrice, int cargo) {
+    public final String selectCheapest(final Port sender, final Port dest, final int maxPrice, final int cargo) {
         SeaTransportation possible = new SeaTransportation(sender, dest, cargo, maxPrice);
         int indexOfShip = getIndexOfNeededShip(cargo);
-        if(indexOfShip >= 0) {
-            if(possible.calculatePrice() < maxPrice) {
+        if (indexOfShip >= 0) {
+            if (possible.calculatePrice() < maxPrice) {
                 possible.calculateTime(availableShips.get(indexOfShip).getSpeed());
                 return "The cheapest: " + possible.toString();
-            } else { 
+            } else {
                 return "There is no such cheap transportation";
             }
         } else {
             return "No available ships";
         }
     }
-        
+
     /**
-     * Select the first fastest available transportation
-     * @return String with description of the transportation
+     * Select the first fastest available transportation.
+     *
+     * @return String with description of the transportation.
      */
-    public String selectFastest(Port sender, Port dest, int cargo) {
+    public final String selectFastest(final Port sender, final Port dest, final int cargo) {
         SeaTransportation possible = new SeaTransportation(sender, dest, cargo);
         int indexOfShip = getIndexOfNeededShip(cargo);
-        if(indexOfShip >= 0) {
+        if (indexOfShip >= 0) {
             int fastestShipIndex = -1;
             int fastest = 0;
-            for(int i = 0; i < availableShips.size(); i++) {
-                if(availableShips.get(i).getSpeed() > fastest) {
+            for (int i = 0; i < availableShips.size(); i++) {
+                if (availableShips.get(i).getSpeed() > fastest) {
                     fastestShipIndex = i;
                 }
             }
@@ -73,33 +78,33 @@ public class TransportationManager {
         } else {
             return "No available ships";
         }
-    }   
-     
+    }
+
     /**
-     * Proceed the transportation
-     * @return true if successful
+     * Proceed the transportation.
+     *
+     * @return true if successful.
      */
-    public boolean doTransportation(Port sender, Port dest, int cargo) {
+    public final boolean doTransportation(final Port sender, final Port dest, final int cargo) {
         int indexOfShip = getIndexOfNeededShip(cargo);
-        if(indexOfShip >= 0) {
+        if (indexOfShip >= 0) {
             transportations.add(new SeaTransportation(sender, dest, cargo));
             availableShips.remove(indexOfShip);
             return true;
         } else {
-            return false;   
+            return false;
         }
     }
-    
-    public ArrayList<Ship> getAvailableShips() {
+
+    public final ArrayList<Ship> getAvailableShips() {
         return this.availableShips;
     }
 
-    public void addShip(Ship obj) {
+    public final void addShip(final Ship obj) {
         this.availableShips.add(obj);
     }
 
-    public ArrayList<SeaTransportation> getTransportations() {
+    public final ArrayList<SeaTransportation> getTransportations() {
         return this.transportations;
     }
 }
-
